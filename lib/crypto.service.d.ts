3c5fc69b2f7ae5b8a82f50a14928878686cdd56c
JasberrajaKingPin <jraja@kingpin.global>
2022-07-02 18:59:50 +0530

    updated package.json

--- lib/crypto.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CryptoService {
-    private key;
-    private iv;
-    encryptUsingAES256(rawInput: any): string;
-    decryptUsingAES256(encryptedString: string): string;
-}
-//# sourceMappingURL=crypto.service.d.ts.map
